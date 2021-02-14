fe94b1106b1d5cb00960453a245ff8f49bee5368
Timur <timur.toktassynov@gmail.com>
2021-02-14 22:03:58 +0000

    added add and remove complete todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./TodoList"
+import React, {useState, useRef, useEffect} from 'react'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    
+    setTodos(prevTodos => {
+      return [...prevTodos, {id: name, name: name, complete: false}]
+    });
+    console.log(localStorage.getItem(LOCAL_STORAGE_KEY))
+    todoNameRef.current.value = null
+  }
+
+  function handleClearFunction() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos = {todos} toggleTodo = {toggleTodo} />
+      <input ref = {todoNameRef} type="text"/>
+      <button onClick = {handleAddTodo}>Add to-do</button>
+      <button onClick = {handleClearFunction}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </>
   );
 }
 
